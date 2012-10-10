e7e71c20926dd607ab6605b44719b95709e01eb2
Daniel Davila <danieldavila001@gmail.com>
2012-10-10 16:01:43 -0300

    Revert "Hola"
    This reverts commit 65dc54f614ccf02d5145b3b1a4ac3e831fc91215.

--- main.lua
+++ main.lua
@@ -2,8 +2,6 @@
 -- HIDE THE STATUS BAR
 --====================================================================--
 
-
---I added a line in main, let's commit!
 display.setStatusBar(display.HiddenStatusBar)
 
 --====================================================================--
